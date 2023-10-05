151c5b29564639c3911704d73e85c93ecdf19e64
shabestari <niloufar2012shabestari@gmail.com>
2023-10-05 21:59:01 +0330

    added mongodb app id and client id

---
+++ app/src/main/java/com/androiddev/diaryapp/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.androiddev.diaryapp.util
+
+object Constants {
+    const val APP_ID = "mydiaryapp-ugdyw"
+    const val CLIENT_ID = "160221082009-r8mv5k01uan5o7l5hrh9snnvbglnhltj.apps.googleusercontent.com"
+}
