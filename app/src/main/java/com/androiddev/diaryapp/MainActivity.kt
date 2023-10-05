132acc98c44c830812b0033f9556f53758bf8e65
shabestari <niloufar2012shabestari@gmail.com>
2023-10-05 22:49:57 +0330

    setting up navigation graph

--- app/src/main/java/com/androiddev/diaryapp/MainActivity.kt
+++ app/src/main/java/com/androiddev/diaryapp/MainActivity.kt
@@ -11,6 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.compose.rememberNavController
+import com.androiddev.diaryapp.navigation.Screen
+import com.androiddev.diaryapp.navigation.SetupNavGraph
 import com.androiddev.diaryapp.ui.theme.DiaryAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +22,11 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
         setContent {
             DiaryAppTheme {
-
+                val navController = rememberNavController()
+                SetupNavGraph(
+                    startDestination = Screen.Authentication.route,
+                    navController = navController
+                )
             }
         }
     }
