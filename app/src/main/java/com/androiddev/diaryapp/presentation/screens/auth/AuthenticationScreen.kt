d965439f50c71c5bffe5a5b030717a32e808bc93
shabestari <niloufar2012shabestari@gmail.com>
2023-10-09 18:25:14 +0330

    fix color of status bar and navigation bar transparent in both dark and light theme

--- app/src/main/java/com/androiddev/diaryapp/presentation/screens/auth/AuthenticationScreen.kt
+++ app/src/main/java/com/androiddev/diaryapp/presentation/screens/auth/AuthenticationScreen.kt
@@ -1,17 +1,21 @@
 package com.androiddev.diaryapp.presentation.screens.auth
 
 import android.annotation.SuppressLint
-import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
 import com.androiddev.diaryapp.util.Constants.CLIENT_ID
 import com.stevdzasan.messagebar.ContentWithMessageBar
 import com.stevdzasan.messagebar.MessageBarState
 import com.stevdzasan.onetap.OneTapSignInState
 import com.stevdzasan.onetap.OneTapSignInWithGoogle
-import java.lang.Exception
+
 
 @ExperimentalMaterial3Api
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -26,14 +30,19 @@ fun AuthenticationScreen(
     onDialogDismissed: (String) -> Unit,
     navigateToHome: () -> Unit
 ) {
-    Scaffold(content = {
-        ContentWithMessageBar(messageBarState = messageBarState, errorMaxLines = 3){
-            AuthenticationContent(
-                loadingState = loadingState,
-                onButtonClicked = onButtonClicked
-            )
-        }
-    })
+    Scaffold(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.surface)
+            .statusBarsPadding()
+            .navigationBarsPadding(),
+        content = {
+            ContentWithMessageBar(messageBarState = messageBarState, errorMaxLines = 3) {
+                AuthenticationContent(
+                    loadingState = loadingState,
+                    onButtonClicked = onButtonClicked
+                )
+            }
+        })
     OneTapSignInWithGoogle(
         state = oneTapState,
         clientId = CLIENT_ID,
@@ -44,8 +53,8 @@ fun AuthenticationScreen(
             onDialogDismissed(message)
         })
 
-    LaunchedEffect(key1 = authenticated){
-        if(authenticated){
+    LaunchedEffect(key1 = authenticated) {
+        if (authenticated) {
             navigateToHome()
         }
     }
