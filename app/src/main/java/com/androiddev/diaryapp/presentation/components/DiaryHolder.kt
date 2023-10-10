92527b64826479aa9a8da19e16d1ab9f68347f70
shabestari <niloufar2012shabestari@gmail.com>
2023-10-10 13:58:41 +0330

    designed the diary holder component

--- app/src/main/java/com/androiddev/diaryapp/presentation/components/DiaryHolder.kt
+++ app/src/main/java/com/androiddev/diaryapp/presentation/components/DiaryHolder.kt
@@ -1,62 +1,161 @@
 package com.androiddev.diaryapp.presentation.components
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.androiddev.diaryapp.model.Diary
+import com.androiddev.diaryapp.model.Mood
+import com.androiddev.diaryapp.ui.theme.Elevation
+import com.androiddev.diaryapp.util.toInstant
+import java.time.Instant
 import java.time.LocalDate
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 @Composable
-fun DateHeader(localDate: LocalDate) {
-    Row(verticalAlignment = Alignment.CenterVertically) {
-        Column(horizontalAlignment = Alignment.End) {
-            Text(
-                text = String.format("%02d", localDate.dayOfMonth),
-                style = TextStyle(
-                    fontSize = MaterialTheme.typography.titleLarge.fontSize,
-                    fontWeight = FontWeight.Light
-                )
-            )
-            Text(
-                text = localDate.dayOfWeek.toString().take(3),
-                style = TextStyle(
-                    fontSize = MaterialTheme.typography.bodySmall.fontSize,
-                    fontWeight = FontWeight.Light
-                )
-            )
-        }
+fun DiaryHolder(diary: Diary, onClick: (String) -> Unit) {
+    val localDensity = LocalDensity.current
+    var componentHeight by remember { mutableStateOf(0.dp) }
+    var galleryOpened by remember { mutableStateOf(false) }
+    Row(
+        modifier = Modifier.clickable(
+            indication = null,
+            interactionSource = remember {
+                MutableInteractionSource()
+            }
+        ) { onClick(diary._id.toString()) }) {
         Spacer(modifier = Modifier.width(14.dp))
-        Column(horizontalAlignment = Alignment.Start) {
-            Text(
-                text = localDate.month.toString().lowercase().replaceFirstChar { it.titlecase() },
-                style = TextStyle(
-                    fontSize = MaterialTheme.typography.titleLarge.fontSize,
-                    fontWeight = FontWeight.Light
-                )
-            )
-            Text(
-                text = "${localDate.year}",
-                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.38f),
-                style = TextStyle(
-                    fontSize = MaterialTheme.typography.bodySmall.fontSize,
-                    fontWeight = FontWeight.Light
-                )
-            )
+        Surface(
+            modifier = Modifier.width(2.dp).height(componentHeight + 14.dp),
+            tonalElevation = Elevation.Level1
+        ) {
+            Spacer(modifier = Modifier.width(20.dp))
+            Surface(
+                modifier = Modifier.clip(shape = Shapes().medium).onGloballyPositioned {
+                    componentHeight = with(localDensity) { it.size.height.toDp() }
+                },
+                tonalElevation = Elevation.Level1
+            ) {
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    DiaryHeader(moodName = diary.mood, time = diary.date.toInstant())
+                    Text(
+                        modifier = Modifier.padding(all = 14.dp),
+                        text = diary.description,
+                        style = TextStyle(fontSize = MaterialTheme.typography.bodyLarge.fontSize),
+                        maxLines = 4,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                    if (diary.images.isNotEmpty()) {
+                        ShowGalleryButton(
+                            galleryOpened = galleryOpened,
+                            onClick = {
+                            galleryOpened = !galleryOpened
+                        })
+                    }
+                    AnimatedVisibility(visible = galleryOpened){
+                        Column(modifier = Modifier.padding(all = 14.dp)) {
+                            Gallery(images = diary.images)
+                        }
+                    }
+                }
+            }
         }
     }
 }
 
 @Composable
+fun DiaryHeader(moodName: String, time: Instant) {
+    val mood by remember { mutableStateOf(Mood.valueOf(moodName)) }
+    val formatter = remember {
+        DateTimeFormatter.ofPattern("hh:mm a", Locale.getDefault())
+            .withZone(ZoneId.systemDefault())
+    }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(mood.containerColor)
+            .padding(horizontal = 14.dp, vertical = 7.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Image(
+                modifier = Modifier.size(18.dp),
+                painter = painterResource(id = mood.icon),
+                contentDescription = "Mood Icon",
+            )
+            Spacer(modifier = Modifier.width(7.dp))
+            Text(
+                text = mood.name,
+                color = mood.contentColor,
+                style = TextStyle(fontSize = MaterialTheme.typography.bodyMedium.fontSize)
+            )
+        }
+        Text(
+            text = formatter.format(time),
+            color = mood.contentColor,
+            style = TextStyle(fontSize = MaterialTheme.typography.bodyMedium.fontSize)
+        )
+    }
+}
+
+@Composable
+fun ShowGalleryButton(
+    galleryOpened: Boolean,
+    /*galleryLoading: Boolean,*/
+    onClick: () -> Unit
+) {
+    TextButton(onClick = onClick) {
+        Text(
+//            text = if (galleryOpened)
+//                if (galleryLoading) "Loading" else "Hide Gallery"
+//            else "Show Gallery",
+            text = if(galleryOpened) "Hide Gallery" else "Show Gallery",
+            style = TextStyle(fontSize = MaterialTheme.typography.bodySmall.fontSize)
+        )
+    }
+}
+
+@Composable
 @Preview
-fun DateHeaderPreview(){
-    DateHeader(localDate = LocalDate.now())
+fun DiaryHolderPreview() {
+    DiaryHolder(diary = Diary().apply {
+        title = "my diary"
+        description =
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        mood = Mood.Happy.name
+    }, onClick = {})
 }
