07bfda1eb57429348705241cfca8cc8a0bfa79bb
shabestari <niloufar2012shabestari@gmail.com>
2023-11-13 13:27:11 +0330

    added migration option to database(but still is not used)

--- app/src/main/java/com/androiddev/diaryapp/data/database/ImagesDatabase.kt
+++ app/src/main/java/com/androiddev/diaryapp/data/database/ImagesDatabase.kt
@@ -1,14 +1,21 @@
 package com.androiddev.diaryapp.data.database
 
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.androiddev.diaryapp.data.database.entity.ImageToDelete
 import com.androiddev.diaryapp.data.database.entity.ImageToUpload
 import com.androiddev.diaryapp.data.database.entity.ImageToUploadDao
 
 @Database(
     entities = [ImageToUpload::class, ImageToDelete::class],
-    version = 2, exportSchema = false
+    version = 1,
+    exportSchema = true,
+//    autoMigrations = [
+//        AutoMigration (from = 1, to = 2)
+//    ]
 )
 abstract class ImagesDatabase : RoomDatabase() {
     abstract fun ImageToUploadDao(): ImageToUploadDao
