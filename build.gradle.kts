b9bf74b7e11596a19d782acb086adb718a8549fb
shabestari <niloufar2012shabestari@gmail.com>
2023-11-19 12:28:38 +0330

    removed google services classpath from gradle.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,6 @@ buildscript{
 //    ext{
         val compose_version = "1.4.6"
 //    }
-    dependencies{
-        classpath("com.google.gms:google-services:4.4.0")
-    }
 }
 plugins {
     id("com.android.application") version "8.1.1" apply false
@@ -15,5 +12,4 @@ plugins {
     id("com.google.dagger.hilt.android") version "2.48" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
     id("com.google.devtools.ksp") version "1.8.20-1.0.11" apply false
-
 }
