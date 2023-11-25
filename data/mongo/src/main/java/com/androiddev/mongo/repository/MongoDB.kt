5a0c14a8e20f2742ce9001c3410576fd46bf30c4
shabestari <niloufar2012shabestari@gmail.com>
2023-11-25 16:43:33 +0330

    fixed the filter query error

--- data/mongo/src/main/java/com/androiddev/mongo/repository/MongoDB.kt
+++ data/mongo/src/main/java/com/androiddev/mongo/repository/MongoDB.kt
@@ -16,6 +16,8 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 import org.mongodb.kbson.ObjectId
+import java.time.LocalDateTime
+import java.time.LocalTime
 import java.time.ZoneId
 import java.time.ZonedDateTime
 
@@ -71,8 +73,20 @@ object MongoDB : MongoRepository {
                 realm.query<Diary>(
                     "ownerId == $0 AND date < $1 AND date > $2",
                     user.id,
-                    RealmInstant.from(zonedDateTime.plusDays(1).toInstant().epochSecond, 0),
-                    RealmInstant.from(zonedDateTime.minusDays(1).toInstant().epochSecond, 0)
+                    RealmInstant.from(
+                        LocalDateTime.of(
+                            zonedDateTime.toLocalDate().plusDays(1),
+                            LocalTime.MIDNIGHT
+                        ).toEpochSecond(zonedDateTime.offset),
+                        0
+                    ),
+                    RealmInstant.from(
+                        LocalDateTime.of(
+                            zonedDateTime.toLocalDate(),
+                            LocalTime.MIDNIGHT
+                        ).toEpochSecond(zonedDateTime.offset),
+                        0
+                    )
                 )
                     .asFlow().map { result ->
                         RequestState.Success(
